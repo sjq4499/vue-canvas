b02ddc43da93fe2640bac10472bac095718ef800
sunjiaqi <sunjiaqi@pxjy.com>
2020-05-27 14:43:36 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Descripttion: 
+ * @Author: sjq
+ * @Date: 2020-04-28 17:13:54
+ * @LastEditors: sjq
+ * @LastEditTime: 2020-05-27 14:42:56
+ -->
+<template>
+  <div id="app">123332</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+
+    }
+  }
+};
+</script>
+
+<style></style>
