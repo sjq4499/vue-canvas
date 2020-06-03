77548689df48db218392ce2dfb0daa53ead167d0
sunjiaqi <sunjiaqi@pxjy.com>
2020-06-03 18:52:32 +0800

    '去掉element'

--- src/main.js
+++ src/main.js
@@ -3,15 +3,11 @@
  * @Author: sjq
  * @Date: 2020-04-28 17:13:54
  * @LastEditors: sjq
- * @LastEditTime: 2020-05-27 14:36:13
+ * @LastEditTime: 2020-06-03 18:52:14
  */
 import Vue from "vue";
 import App from "./App.vue";
 
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
